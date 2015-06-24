c39848ff7e366ea1e86e714bed87edd1a3d8ef3b
Steven Abrams <steven.abrams86@gmail.com>
2015-06-23 22:22:46 -0600

    added path matching algorithm

--- spec/match_path_spec.js
+++ spec/match_path_spec.js
@@ -8,8 +8,8 @@ describe('matchpath', function(){
       {verb: 'get', path: '/you'},
       {verb: 'post', path: '/me'},
     ];
-    var actual = matchPath(routeDefinitions, 'get', '/me');
-    var expectedOutput = {verb: 'get', path: '/me'};
+    var actual = matchPath(routeDefinitions, 'get', '/you');
+    var expectedOutput = {verb: 'get', path: '/you'};
     expect(actual).toEqual(expectedOutput);
   });
 
@@ -24,4 +24,35 @@ describe('matchpath', function(){
     expect(actual).toEqual(expectedOutput);
   });
 
+  it('returns null if all arguments arent given', function(){
+    var routeDefinitions = [
+      {verb: 'get', path: '/me'},
+      {verb: 'get', path: '/you'},
+      {verb: 'post', path: '/you'},
+    ];
+    var actual = matchPath(routeDefinitions, 'get');
+    var expectedOutput = null;
+    expect(actual).toEqual(expectedOutput);
+  });
+
+  it('returns a dynamically matched object', function(){
+    var routeDefinitions = [
+      { verb: 'get', path: '/artists/:artist_name/albums' },
+      { verb: 'get', path: '/artists/:artist_name/albums/:album_id' },
+      { verb: 'get', path: '/:name' },
+    ];
+    var expectedOutput = { verb: 'get', path: '/:name' };
+    var actual = matchPath(routeDefinitions, 'get', '/steve');
+    expect(actual).toEqual(expectedOutput);
+  });
+
+  it('returns the first matched object', function () {
+    var routeDefinitions = [
+      { verb: 'get', path: '/:id' },
+      { verb: 'get', path: '/new' },
+    ];
+    var expectedOutput = { verb: 'get', path: '/:id' };
+    var actual = matchPath(routeDefinitions, 'get', '/new');
+    expect(actual).toEqual(expectedOutput);
+  });
 });
